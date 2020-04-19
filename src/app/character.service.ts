c5fb1d97669bc18a4995ce9b20bbedcad825136f
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-04-19 18:19:52 +0300

    Move AbilityScore.ts to interface folder.

--- src/app/character.service.ts
+++ src/app/character.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AbilityScore } from './sheet/AbilityScore';
+import { AbilityScore } from './interface/AbilityScore';
 
 @Injectable({
   providedIn: 'root'
--- src/app/sheet/attribute-field/attribute-field.component.ts
+++ src/app/sheet/attribute-field/attribute-field.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {AbilityScore} from '../AbilityScore';
+import {AbilityScore} from '../../interface/AbilityScore';
 
 @Component({
   selector: 'app-attribute-field',
