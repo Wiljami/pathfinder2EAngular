72e267fcc68253ac0c9caa7325fe8c449f0d326e
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-04-25 15:33:58 +0300

    Add placeholder dummy skill for all the stats.

--- src/app/sheet/skills-field/skills-field.component.ts
+++ src/app/sheet/skills-field/skills-field.component.ts
@@ -5,7 +5,12 @@ import {CharacterService} from '../../character.service';
   selector: 'app-skills-field',
   template: `
     <h1>skills field</h1>
-    <p>Placeholder skill: + {{character.dex.modifier}}</p>
+    <p>Placeholder STR skill: + {{character.str.value}}</p>
+    <p>Placeholder DEX skill: + {{character.dex.value}}</p>
+    <p>Placeholder CON skill: + {{character.con.value}}</p>
+    <p>Placeholder INT skill: + {{character.int.value}}</p>
+    <p>Placeholder WIS skill: + {{character.wis.value}}</p>
+    <p>Placeholder CHA skill: + {{character.cha.value}}</p>
   `
 })
 export class SkillsFieldComponent implements OnInit {
