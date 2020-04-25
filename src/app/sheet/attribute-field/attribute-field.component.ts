ac59ee3cae8833c45491eebb389d3bf400de93c8
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-04-25 15:34:26 +0300

    Add input fields for the attributes.

--- src/app/sheet/attribute-field/attribute-field.component.ts
+++ src/app/sheet/attribute-field/attribute-field.component.ts
@@ -5,14 +5,21 @@ import {AbilityScore} from '../../interface/AbilityScore';
   selector: 'app-attribute-field',
   template: `
     <p>{{stat.name}}: {{stat.value}}</p>
+    <input type="number" matInput [(ngModel)]="value" (change)="change()">
   `
 })
 export class AttributeFieldComponent implements OnInit {
   @Input() stat: AbilityScore;
+  value;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.value = this.stat.value;
   }
 
+  change() {
+    console.log(this.value);
+    this.stat.value = this.value;
+  }
 }
