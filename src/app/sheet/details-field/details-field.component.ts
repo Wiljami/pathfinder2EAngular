ad36250c747a19623e7adb3c4c0828435d5d45b5
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-04-29 18:44:18 +0300

    Implement name and level changes for the details-field.

--- src/app/sheet/details-field/details-field.component.ts
+++ src/app/sheet/details-field/details-field.component.ts
@@ -9,7 +9,7 @@ import {CharacterService} from '../../services/character.service';
         <mat-grid-tile colspan="4">
           <mat-form-field class="fullwidth">
             <mat-label>Character name</mat-label>
-            <input matInput/>
+            <input type="text" [(ngModel)]="name" matInput (change)="changeName($event)">
           </mat-form-field>
         </mat-grid-tile>
         <mat-grid-tile colspan="4">
@@ -21,7 +21,7 @@ import {CharacterService} from '../../services/character.service';
         <mat-grid-tile colspan="2">
           <mat-form-field class="halfwidth">
             <mat-label>Level</mat-label>
-            <input type="number" [(ngModel)]="level" matInput [min]="1" [max]="20"/>
+            <input type="number" [(ngModel)]="level" matInput [min]="1" [max]="20" (change)="changeLevel($event)">
           </mat-form-field>
         </mat-grid-tile>
         <mat-grid-tile colspan="4">
@@ -73,19 +73,30 @@ import {CharacterService} from '../../services/character.service';
     '.tablewidth { min-width: 800px; width: 100%}',
     '.fullwidth { width: 100%; min-width: 300px;}',
     '.halfwidth { width: 50%; min-width: 150px;}',
-    '.quarterwidth { width: 25%; min-width: 75px;}',
+    '.quarterwidth { width: 25%; min-width: 75px;}'
   ]
 })
 export class DetailsFieldComponent implements OnInit, DoCheck {
   level: number;
+  name: string;
 
   constructor(public character: CharacterService) { }
 
   ngOnInit(): void {
     this.level = this.character.level;
+    this.name = this.character.name;
   }
 
   ngDoCheck(): void {
     this.level = this.character.level;
+    this.name = this.character.name;
+  }
+
+  changeName(event) {
+    this.character.name = event.target.value;
+  }
+
+  changeLevel(event) {
+    this.character.level = event.target.value;
   }
 }
