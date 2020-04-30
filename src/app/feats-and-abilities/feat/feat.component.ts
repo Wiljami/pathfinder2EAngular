22dbd0482766239e12affbbdd655b45f3ee74970
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-04-30 23:17:32 +0300

    Remove unneeded variable.

--- src/app/feats-and-abilities/feat/feat.component.ts
+++ src/app/feats-and-abilities/feat/feat.component.ts
@@ -9,20 +9,24 @@ import { Component, OnInit } from '@angular/core';
           <h3>Special 1st</h3>
         </mat-panel-title>
       </mat-expansion-panel-header>
-      <mat-form-field>
-        <input matInput placeholder="Name">
+      <mat-form-field class="fullwidth">
+        <input matInput placeholder="Feat Name">
       </mat-form-field>
-      <mat-form-field>
-        <input matInput placeholder="Age">
+      <mat-form-field class="fullwidth">
+        <input matInput placeholder="Type">
+      </mat-form-field>
+      <mat-form-field class="fullwidth">
+        <textarea matInput placeholder="Description"></textarea>
       </mat-form-field>
     </mat-expansion-panel>
   `,
   styles: [
+    '.fullwidth { width: 100%; min-width: 300px;}',
+    '.halfwidth { width: 50%; min-width: 150px;}',
+    '.quarterwidth { width: 25%; min-width: 75px;}'
   ]
 })
 export class FeatComponent implements OnInit {
-  panelOpenState = false;
-
   constructor() { }
 
   ngOnInit(): void {
