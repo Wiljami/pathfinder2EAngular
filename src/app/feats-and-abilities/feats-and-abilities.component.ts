b5d1de86665284a107875d41ff23c8ebb0389c1a
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-04-30 23:11:33 +0300

    Add css style for two column style to the feats tab.

--- src/app/feats-and-abilities/feats-and-abilities.component.ts
+++ src/app/feats-and-abilities/feats-and-abilities.component.ts
@@ -4,11 +4,27 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-feats-and-abilities',
   template: `
     <h1>Feats and abilities</h1>
-    <app-feat></app-feat>
-    <app-feat></app-feat>
-    <app-feat></app-feat>
-    <app-feat></app-feat>
-  `
+    <div class="row">
+      <div class="column">
+        <h2>Ancestry Feats and Abilities</h2>
+        <app-feat></app-feat>
+        <app-feat></app-feat>
+        <app-feat></app-feat>
+        <app-feat></app-feat>
+      </div>
+      <div class="column">
+        <h2>Class Feats and Abilities</h2>
+        <app-feat></app-feat>
+        <app-feat></app-feat>
+        <app-feat></app-feat>
+        <app-feat></app-feat>
+      </div>
+    </div>
+  `,
+  styles: [
+    '.row {display: flex;}',
+    '.column {flex: 50%; padding: 10px}'
+  ]
 })
 export class FeatsAndAbilitiesComponent implements OnInit {
 
