27e98ad6daf7830513d8bdf4c50be0baf8f8be7d
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-05-10 19:49:31 +0300

    Adjust button mat.

--- src/app/feats-and-abilities/feats-field/feats-field.component.ts
+++ src/app/feats-and-abilities/feats-field/feats-field.component.ts
@@ -6,15 +6,18 @@ import {Feat} from '../../services/Feat';
   selector: 'app-feats-field',
   template: `
     <h2>{{title}}</h2>
-    <button type="button" (click)="addFeat()">Add Feat</button>
-    <button type="button" (click)="lockSheet()">{{lockButtonText}}</button>
+    <button mat-stroked-button type="button" (click)="addFeat()">Add Feat</button>
+    <button mat-stroked-button type="button" (click)="lockSheet()">{{lockButtonText}}</button>
 
     <div cdkDropListDisabled="{{listLocked}}" cdkDropList (cdkDropListDropped)="drop($event)">
       <div cdkDrag [cdkDragData]="feat" *ngFor="let feat of feats">
         <app-feat [feat]="feat" [callBack]="removeFeat" [locked]="listLocked"></app-feat>
       </div>
     </div>
-  `
+  `,
+  styles: [
+    ''
+  ]
 })
 export class FeatsFieldComponent implements OnInit {
   @Input() title: string;
